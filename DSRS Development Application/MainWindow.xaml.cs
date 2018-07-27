410feb50082007c0bce5c3f21b8d1550cff31776
PheonixVX <TheGrimReaper.VX@gmail.com>
2018-07-26 23:42:47 -0400

    Added Better Save Functionality

--- DSRS Development Application/MainWindow.xaml.cs
+++ DSRS Development Application/MainWindow.xaml.cs
@@ -47,16 +47,14 @@ namespace DSRS_Development_Application
             NProject.Visibility = Visibility.Collapsed;
             JProject.Visibility = Visibility.Collapsed;
             HProject.Visibility = Visibility.Collapsed;
-            IDE.Text = "print 'Hello World!'\n\n" + "print 'Press Enter to Exit'\n" + "#KEEP PYTHON OPEN\n" + "raw_input()";
+            BUILDJ.Visibility = Visibility.Collapsed;
+            BUILDJR.Visibility = Visibility.Collapsed;
+            BUILDH.Visibility = Visibility.Collapsed;
+            BUILDHF.Visibility = Visibility.Collapsed;
+            IDE.Text = "print('Hello World!')\n\n" + "print('Press Enter to Exit')\n" + "#KEEP PYTHON OPEN\n" + "raw_input()";
             BUILD.Visibility = Visibility.Visible;
             READONLY.Visibility = Visibility.Visible;
             SAVE.Visibility = Visibility.Visible;
-            string path = Environment.CurrentDirectory + "/" + "PyFile.py";
-            if (!System.IO.File.Exists(path))
-            {
-                System.IO.File.CreateText(path);
-                MessageBox.Show("Python Project Created Successfully, Projects can be found in the working directory");
-            }
         }
         private void RD_Click(object sender, RoutedEventArgs e)
         {
@@ -69,21 +67,21 @@ namespace DSRS_Development_Application
         }
         private void BA_Click(object sender, RoutedEventArgs e)
         {
+            OpenFileDialog OFD = new OpenFileDialog();
+            OFD.Title = "What is the Python File you would like to Build?";
+            OFD.ShowDialog();
             ProcessStartInfo startInfo = new ProcessStartInfo();
             startInfo.FileName = "Python.exe";
-            startInfo.Arguments = "PyFile.py";
+            startInfo.Arguments = OFD.FileName;
             Process.Start(startInfo.FileName, startInfo.Arguments);
         }
-
         private void SAVE_Click(object sender, RoutedEventArgs e)
         {
-            string path = Environment.CurrentDirectory + "/" + "PyFile.py";
-            using (StreamWriter sw = new StreamWriter(path))
-            {
-                sw.WriteLine(IDE.Text);
-            }
+            SaveFileDialog SFD = new SaveFileDialog();
+            SFD.Filter = "HTML (*.html)|*.htm; *.html| Java (*.java)|*.java;|Python (*.py)|*.py| All files (*.*)|*.*";
+            if (SFD.ShowDialog() == true)
+                System.IO.File.WriteAllText(SFD.FileName, IDE.Text);
         }
-
         private void Java_Click(object sender, RoutedEventArgs e)
         {
             UpdateF.Visibility = Visibility.Collapsed;
@@ -92,32 +90,26 @@ namespace DSRS_Development_Application
             NProject.Visibility = Visibility.Collapsed;
             JProject.Visibility = Visibility.Collapsed;
             HProject.Visibility = Visibility.Collapsed;
+            BUILDJ.Visibility = Visibility.Collapsed;
+            BUILDJR.Visibility = Visibility.Collapsed;
+            BUILDH.Visibility = Visibility.Collapsed;
+            BUILDHF.Visibility = Visibility.Collapsed;
             IDE.Text = "/* JAVA FILE */\n\n" + "public class JFile\n" + "{\n" + " public static void main(String[] args) {\n" + "/* IN WPF YOU CAN NOT HAVE DOUBLE QUOTES INSIDE DOUBLE QUOTES SO YOU WILL MANUALLY HAVE TO EDIT THE HELLO WORLD CODE SO CHANGE THE SINGLE QUOTES TO DOUBLE QUOTES BEFORE AND AFTER TO FIX THE PROJECT */\n" + "  System.out.println('Hello World!');\n" + " }\n" + "}";
             BUILDJ.Visibility = Visibility.Visible;
             BUILDJR.Visibility = Visibility.Visible;
             READONLY.Visibility = Visibility.Visible;
-            SAVEJ.Visibility = Visibility.Visible;
-            string path = Environment.CurrentDirectory + "/" + "JFile.java";
-            if (!System.IO.File.Exists(path))
-            {
-                System.IO.File.CreateText(path);
-                MessageBox.Show("Java Project Created Successfully, Projects can be found in the working directory ");
-            }
+            SAVE.Visibility = Visibility.Visible;
         }
         private void BJ_Click(object sender, RoutedEventArgs e)
         {
+            OpenFileDialog OFD = new OpenFileDialog();
+            OFD.Title = "What Java File Would you like to compile?";
+            OFD.Filter = "Java File (*.java)|*.java";
+            OFD.ShowDialog();
             ProcessStartInfo startInfo = new ProcessStartInfo();
             startInfo.FileName = "C:/Program Files/Java/jdk-10/bin/javac.exe";
-            startInfo.Arguments = "JFile.java";
+            startInfo.Arguments = OFD.FileName;
             Process.Start(startInfo.FileName, startInfo.Arguments);
-        }
-        private void SAVEJ_Click(object sender, RoutedEventArgs e)
-        {
-            string path = Environment.CurrentDirectory + "/" + "JFile.java";
-            using (StreamWriter sw = new StreamWriter(path))
-            {
-                sw.WriteLine(IDE.Text);
-            }
         }
         private void JR_Click(object sender, RoutedEventArgs e)
         {
@@ -134,39 +126,37 @@ namespace DSRS_Development_Application
             NProject.Visibility = Visibility.Collapsed;
             JProject.Visibility = Visibility.Collapsed;
             HProject.Visibility = Visibility.Collapsed;
+            BUILDJ.Visibility = Visibility.Collapsed;
+            BUILDJR.Visibility = Visibility.Collapsed;
+            BUILDH.Visibility = Visibility.Collapsed;
+            BUILDHF.Visibility = Visibility.Collapsed;
             IDE.Text = "<!DOCTYPE html>\n" + "<html>\n" + " <head>\n" + "  <title>\n" + "   HTML\n" + "  </title>\n" + " </head>\n" + "  <body>\n" + "  </body>\n" + "<html>\n";
             BUILDH.Visibility = Visibility.Visible;
             BUILDHF.Visibility = Visibility.Visible;
             READONLY.Visibility = Visibility.Visible;
-            SAVEH.Visibility = Visibility.Visible;
-            string path = Environment.CurrentDirectory + "/" + "Index.html";
-            if (!System.IO.File.Exists(path))
-            {
-                System.IO.File.CreateText(path);
-                MessageBox.Show("HTML Project Created Successfully, Projects can be found in the working directory");
-            }
+            SAVE.Visibility = Visibility.Visible;
         }
         private void BH_Click(object sender, RoutedEventArgs e)
         {
+            OpenFileDialog OFD = new OpenFileDialog();
+            OFD.Title = "What HTML File Would you like to compile?";
+            OFD.Filter = "HTML File (*.html) (*.htm)|*.html; *.htm";
+            OFD.ShowDialog();
             ProcessStartInfo startInfo = new ProcessStartInfo();
             startInfo.FileName = "chrome";
-            startInfo.Arguments = "Index.html";
+            startInfo.Arguments = OFD.FileName;
             Process.Start(startInfo.FileName, startInfo.Arguments);
         }
         private void BHF_Click(object sender, RoutedEventArgs e)
         {
+            OpenFileDialog OFD = new OpenFileDialog();
+            OFD.Title = "What HTML File Would you like to compile?";
+            OFD.Filter = "HTML File (*.htm) (*.html)|*.html; *.htm";
+            OFD.ShowDialog();
             ProcessStartInfo startInfo = new ProcessStartInfo();
             startInfo.FileName = "firefox";
-            startInfo.Arguments = "Index.html";
+            startInfo.Arguments = OFD.FileName;
             Process.Start(startInfo.FileName, startInfo.Arguments);
-        }
-        private void SAVEH_Click(object sender, RoutedEventArgs e)
-        {
-            string path = Environment.CurrentDirectory + "/" + "Index.html";
-            using (StreamWriter sw = new StreamWriter(path))
-            {
-                sw.WriteLine(IDE.Text);
-            }
         }
         private void Update_Click_Chrome(object sender, RoutedEventArgs e)
         {
